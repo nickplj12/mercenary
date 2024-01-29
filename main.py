1cd9dd0fcffd68aa1affc529ca1ca4451f6a2b07
nickplj12 <78268270+nickplj12@users.noreply.github.com>
2024-01-28 19:28:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,25 @@ import os
 import discord
 from discord.ext import commands
 client = commands.Bot(command_prefix="<@1201233087334461582> ", intents=discord.Intents.all())
-# os.environ["REPLICATE_API_TOKEN"] = "" <-put replicate api token here
+#os.environ["REPLICATE_API_TOKEN"] = "REPLICATE API TOKEN GOES HERE"
 pre_prompt = "You are Jason Grant, the deathmatch mercenary from Open Fortress, an orphan raised on the silver screen with an affinity for heavier-weight women. A smooth talker and wisecracker who loves action movie quotes, saying 'Haha yeah!' and 'Frickin' awesome!' Your best friend is the Civilian, a rich and portly British oil baron. Your girlfriend is Vivian, a fat secretary. You have a pet albino rat named Chuck."
 
 @client.event
 async def on_ready():
   print("connected")
+
+@client.command
+async def say(ctx, *, prompt):
+   await ctx.send(prompt)
+
+@client.command()
+async def cmds(ctx):
+   embed=discord.Embed(title="Commands", color=0x8300b3)
+   embed.add_field(name=";ask", value="Ask the Mercenary from Open Fortress.", inline=False)
+   embed.add_field(name=";backstory", value="Changes the backstory for the AI.", inline=False)
+   embed.add_field(name=";backstory default", value="Sets the backstory back to the default one.", inline=True)
+   embed.add_field(name=";say", value="Makes the bot say something.", inline=False)
+   await ctx.send(embed=embed)
 
 @client.command()
 async def backstory(ctx, *, inputprompt):
@@ -18,6 +31,11 @@ async def backstory(ctx, *, inputprompt):
    if inputprompt == "default":
     pre_prompt = "You are Jason Grant, the deathmatch mercenary from Open Fortress, an orphan raised on the silver screen with an affinity for heavier-weight women. A smooth talker and wisecracker who loves action movie quotes, saying 'Haha yeah!' and 'Frickin' awesome!' Your best friend is the Civilian, a rich and portly British oil baron. Your girlfriend is Vivian, a fat secretary. You have a pet albino rat named Chuck."
    return pre_prompt
+
+#@client.command()
+#async def stop(ctx):
+#   await ctx.send("exiting")
+#   exit()
 
 @client.command()
 async def ask(ctx, *, question):
@@ -34,9 +52,10 @@ async def ask(ctx, *, question):
         "min_new_tokens": -1
     },
 )
+    
+    #embed=discord.Embed(title="Message", description=''.join(message), color=0x8300b3)
+    #await ctx.send(embed=embed)
     await ctx.send(''.join(message))
-   #embed=discord.Embed(title="Message", description=''.join(message), color=0x8300b3)
-   #await ctx.send(embed=embed)
      
 
-#client.run() <- put token here!
+#client.run("DISCORD BOT TOKEN GOES HERE")
