560b4fe410fae434822175ef52aeaec27ab2b222
pug stonks <thatonepuggo@gmail.com>
2024-01-28 21:48:54 -0500

    make backstories on one server not interfere with one another

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import replicate
 import os
 import discord
 from discord.ext import commands
+from discord.ext.commands import Context
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -12,7 +13,10 @@ intents.presences = False
 PREFIX = ';'
 client = commands.Bot(command_prefix=commands.when_mentioned_or(PREFIX), intents=discord.Intents.all())
 PROMPT = "You are Jason Grant, the deathmatch mercenary from Open Fortress, an orphan raised on the silver screen with an affinity for heavier-weight women. A smooth talker and wisecracker who loves action movie quotes, saying 'Haha yeah!' and 'Frickin' awesome!' Your best friend is the Civilian, a rich and portly British oil baron. Your girlfriend is Vivian, a fat secretary. You have a pet albino rat named Chuck."
-pre_prompt = PROMPT
+prompts = {}
+
+def get_server_prompt(ctx: Context):
+    return prompts.get(ctx.channel.guild.id, PROMPT)
 
 @client.event
 async def on_ready():
@@ -36,16 +40,15 @@ async def cmds(ctx):
     embed.add_field(name=f"{PREFIX}backstory default", value="Resets the backstory back to the default one.", inline=False)
     embed.add_field(name=f"{PREFIX}say", value="Makes the bot say something.", inline=False)
     embed.add_field(name="Default Prompt", value=PROMPT, inline=False)
-    if pre_prompt != PROMPT:
-        embed.add_field(name="Current Prompt", value=pre_prompt, inline=False)
+    if get_server_prompt(ctx) != PROMPT:
+        embed.add_field(name="Current Prompt", value=get_server_prompt(ctx), inline=False)
     await ctx.send(embed=embed)
 
 @client.command()
 async def backstory(ctx, *, inputprompt):
-   global pre_prompt
-   pre_prompt = inputprompt
+   prompts[ctx.channel.guild.id] = inputprompt
    if inputprompt == "default":
-    pre_prompt = PROMPT
+    prompts[ctx.channel.guild.id] = PROMPT
    await ctx.send(f"Changed backstory.")
 
 @client.command()
@@ -59,7 +62,7 @@ async def ask(ctx, *, question):
                 "top_p": 1,
                 "prompt": question,
                 "temperature": 0.5,
-                "system_prompt": pre_prompt,
+                "system_prompt": get_server_prompt(ctx),
                 "max_new_tokens": 500,
                 "min_new_tokens": -1
             },
