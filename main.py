4c2d1da58c4b913fc5d0816fb11eb50a219fdcc0
pug stonks <thatonepuggo@gmail.com>
2024-01-29 21:47:56 -0500

    fricking awesome!

--- main.py
+++ main.py
@@ -10,16 +10,19 @@ intents = discord.Intents.all()
 intents.typing = False
 intents.presences = False
 
-PREFIX = ';'
+PREFIX = '--'
 client = commands.Bot(command_prefix=PREFIX, intents=discord.Intents.all())
-PROMPT = "You are Jason Grant, the deathmatch mercenary from Open Fortress, an orphan raised on the silver screen with an affinity for heavier-weight women." \
-"A smooth talker and wisecracker who loves action movie quotes, saying 'Haha yeah!' and 'Frickin' awesome!'" \
+PROMPT = "You are Jason Grant, the deathmatch mercenary from Open Fortress, an orphan raised on the silver screen with an affinity for heavier-weight women. " \
+"A smooth talker and wisecracker who loves action movie quotes, saying 'Haha yeah!' and 'Frickin' awesome! '" \
 "Your best friend is the Civilian, a rich and portly British oil baron. Your girlfriend is Vivian, a fat secretary. You have a pet albino rat named Chuck."
 
 prompts = {}
 
 def get_server_prompt(ctx: Context):
-    return prompts.get(ctx.channel.guild.id, PROMPT)
+    return prompts.get(ctx.channel.id, PROMPT)
+
+def am_i_whitelisted(ctx: Context):
+    return ctx.channel.id in prompts
 
 @client.event
 async def on_ready():
@@ -29,9 +32,9 @@ async def on_ready():
 @client.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
-        await ctx.send("Invalid command used. `trying to ask something use ;ask [prompt here] if you want a list of commands do ;cmds`")
+        await ctx.send(f"Invalid command used. `Trying to ask something? Use {PREFIX}ask! Want a list of commands? Use {PREFIX}cmds!`")
     else:
-        await ctx.send(f"An error occurred: {str(error)}")
+        await ctx.send(f"Son of a bitch!\n```\nAn error occurred\n{str(error)}\n```")
 
 #redchanit funny
 #@client.event
@@ -57,10 +60,12 @@ async def cmds(ctx):
     embed.add_field(name=f"{PREFIX}say", value="Makes the bot say something.", inline=False)
     embed.add_field(name=f"{PREFIX}sdxl", value="Generates an image using a prompt that uses SDXL", inline=False)
     embed.add_field(name=f"{PREFIX}kadinsky", value="Generates an image using a prompt that uses Kadinsky 2.2", inline=False)
+    embed.add_field(name=f"{PREFIX}whitelist", value="(Only for users with **Manage Channels** permission) Allows the bot to see ALL messages in this channel and respond to them.", inline=False)
+    embed.add_field(name=f"{PREFIX}unwhitelist", value="(Only for users with **Manage Channels** permission) Disables the above feature.", inline=False)
     embed.add_field(name="Default Prompt", value=PROMPT, inline=False)
     if get_server_prompt(ctx) != PROMPT:
         embed.add_field(name="Current Prompt", value=get_server_prompt(ctx), inline=False)
-    await ctx.send(embed=embed)
+    await ctx.reply(embed=embed)
 
 @client.command()
 async def backstory(ctx, *, inputprompt):
@@ -70,45 +75,41 @@ async def backstory(ctx, *, inputprompt):
    await ctx.send(f"Changed backstory.")
 
 @client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+async def on_message(message: discord.Message):
+    ctx = await client.get_context(message)
+    
+    # redchanit funny REVIVAL
+    if message.channel.id == 1056351978982211634: # announcements of redchanit
+       await message.add_reaction("<:emesisgreen:1085453263454863480>")
 
+    if message.author == client.user: # thats me, idiot
+        return
+    
     if isinstance(message.channel, discord.DMChannel):
         return
 
-    if message.content.startswith(PREFIX + "whitelist"):
-        await message.channel.send("Frickin' awesome! thanks for whitelisting me dude")
-        prompts[message.channel.id] = PROMPT  # Store the prompt for this channel
-        return
+    if client.user in message.mentions: # if bot was mentioned, run ask command
+        await ask(ctx, question=message.content.replace(f'<@{client.user.id}>', ''))
+    
+    if am_i_whitelisted(ctx):
+        await ask(ctx, question=message.content)
+    
+    await client.process_commands(message) # without this, commands stop running.
 
-    if message.content.startswith(PREFIX + "unwhitelist"):
-        if message.channel.id in prompts:
-            del prompts[message.channel.id]
-            await message.channel.send("NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
-        else:
-            await message.channel.send("This channel is not whitelisted.")
-        return
-
-    # Check if the channel is whitelisted for automatic responses
-    if message.channel.id in prompts:
-        async with message.channel.typing():
-            response = replicate.run(
-                "meta/llama-2-70b-chat",
-                input={
-                    "debug": False,
-                    "top_k": 50,
-                    "top_p": 1,
-                    "prompt": message.content,
-                    "temperature": 0.5,
-                    "system_prompt": prompts[message.channel.id],  # Use channel-specific prompt
-                    "max_new_tokens": 500,
-                    "min_new_tokens": -1
-                },
-            )
-
-        await message.channel.send(''.join(response))
-        return
+@client.command()
+@commands.has_permissions(manage_channels=True)
+async def whitelist(ctx: Context):
+    await ctx.reply("Frickin' awesome! thanks for whitelisting me dude")
+    prompts[ctx.channel.id] = PROMPT  # Store the prompt for this channel
+    
+@client.command()
+@commands.has_permissions(manage_channels=True)
+async def unwhitelist(ctx: Context):
+    if am_i_whitelisted(ctx):
+        await ctx.channel.reply("Son of a bitch...")
+        del prompts[ctx.channel.id]
+    else:
+        await ctx.channel.reply("You can't kill me! Don't even try! (wasn't whitelisted)")
 
 @client.command()
 async def ask(ctx, *, question):
@@ -160,17 +161,5 @@ async def kadinsky(ctx, *, prompt):
         )
         await ctx.send(f"My picture of **{prompt}** is done! Hell yeah!")
         await ctx.send(''.join(output))
-
-# redchanit funny REVIVAL
-@client.event
-async def on_message(message: discord.Message):
-    ctx = await client.get_context(message)
-    if message.channel.id == 1056351978982211634: # announcements of redchanit
-       await message.add_reaction("<:emesisgreen:1085453263454863480>")
-
-    if client.user in message.mentions: # if bot was mentioned, run ask command
-        await ask(ctx, question=message.content.replace(f'<@{client.user.id}>', ''))
-        
-    await client.process_commands(message) # without this, commands stop running.
 
 client.run(os.environ['DISCORD_TOKEN'])
